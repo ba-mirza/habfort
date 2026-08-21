8388753c082c600f5f2b9adb75ca25f5ecb07f5d
Almas <almas1610@gmail.com>
2026-08-21 18:06:04 +0500

    Import AuthModule in EconomyModule so the guard can resolve

--- habfort-back/src/economy/economy.module.ts
+++ habfort-back/src/economy/economy.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { EconomyController } from './economy.controller';
 
 @Module({
+  imports: [AuthModule],
   controllers: [EconomyController],
 })
 export class EconomyModule {}
